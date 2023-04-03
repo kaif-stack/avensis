fdacbb147763c3a42141f9511227de74ac31046e
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-04-03 17:41:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from "../src/Pages/Home";
+
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/common/Navbar'
+import Footer from '../../components/common/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
